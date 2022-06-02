460eb6b469a714568bbeb1c85f6e5e66305f7b94
Ethan Leisinger <packruler@gmail.com>
2022-06-02 01:14:06 +0000

    Add logger and improve structure

---
+++ pluginutils.go
@@ -0,0 +1,4 @@
+// Package pluginutils is a collection of utilities I use frequently in
+// my Traefik plugins. This is intended to limit the amount of
+// duplicate code in the packages I maintain.
+package pluginutils
